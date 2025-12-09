bfee5b47781498f71cc2b238337b074ecb6a02b5
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2025-12-09 14:21:20 +0600

    Update QR reader.py

--- pages/QR reader.py
+++ pages/QR reader.py
@@ -4,6 +4,7 @@ import numpy as np
 import cv2
 from cryptography.fernet import Fernet
 import json
+from same_content import side_note, social_links
 
 st.set_page_config(
     page_title="Encrypted QR Scanner",
@@ -99,73 +100,8 @@ if st.button("Decrypt QR"):
             st.error(f"Decrypt failed: {e}")
 
 st.markdown("> <span style='color: orange;'>Scanner synced with generator ✅</span>", unsafe_allow_html=True)
+st.markdown('<div style="text-align: center; color: grey;">&copy; 2025 Sakib Hossain Tahmid. All Rights Reserved.</div>', unsafe_allow_html=True) 
 
-
-st.sidebar.markdown(
-    """
-    <style>
-        .social-icons {
-            text-align: center;
-            margin-top: 40px;
-        }
-
-        .social-icons a {
-            text-decoration: none !important;
-            margin: 0 10px;
-            font-size: 20px;
-            display: inline-block;
-            color: inherit !important; /* force child i to use its color */
-        }
-
-        
-
-        /* Hover glitch animation */
-        .social-icons a:hover {
-            animation: glitch 0.3s infinite;
-        }
-
-        
-        /* Contact us heading */
-        .contact-heading {
-            text-align: center;
-            font-size: 22px;
-            font-weight: bold;
-            margin-bottom: 15px;
-        }
-        @keyframes glitch {
-            0% { transform: translate(0px, 0px); text-shadow: 2px 2px #0ff, -2px -2px #f0f; }
-            20% { transform: translate(-2px, 1px); text-shadow: -2px 2px #0ff, 2px -2px #f0f; }
-            40% { transform: translate(2px, -1px); text-shadow: 2px -2px #0ff, -2px 2px #f0f; }
-            60% { transform: translate(-1px, 2px); text-shadow: -2px 2px #0ff, 2px -2px #f0f; }
-            80% { transform: translate(1px, -2px); text-shadow: 2px -2px #0ff, -2px 2px #f0f; }
-            100% { transform: translate(0px, 0px); text-shadow: 2px 2px #0ff, -2px -2px #f0f; }
-        }
-    </style>
-    <div class="social-icons">
-    <div class="contact-heading">Contact Us:</div>
-        <a class='fb' href='https://www.facebook.com/sakibhossain.tahmid' target='_blank'>
-            <i class='fab fa-facebook'></i> 
-        </a> 
-        <a class='insta' href='https://www.instagram.com/_sakib_000001' target='_blank'>
-            <i class='fab fa-instagram'></i> 
-        </a> 
-        <a class='github' href='https://github.com/sakib-12345' target='_blank'>
-            <i class='fab fa-github'></i> 
-        </a> 
-        <a class='email' href='mailto:sakibhossaintahmid@gmail.com'>
-            <i class='fas fa-envelope'></i> 
-        </a>
-    </div>
-    
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    """,
-    unsafe_allow_html=True
-)
-
-
-st.markdown(
-            f'<div style="text-align: center; color: grey;">&copy; 2025 Sakib Hossain Tahmid. All Rights Reserved.</div>',
-            unsafe_allow_html=True
-
-           ) 
-
+with st.sidebar:
+    side_note()
+    social_links()
